70d1e046d698e6bc31cc0a22ee10be05ce9e5c7d
我是小杨我就这样 <913797467@qq.com>
2020-05-20 19:08:46 +0800

    Add files via upload

---
+++ BookManager/urls.py
@@ -0,0 +1,30 @@
+"""BookManager URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from Book import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/', views.index, name='index'),
+    path('study/', views.study, name='study'),
+    path('books/', views.show_books, name="show_books"), # 显示图书信息
+    path('books/<int:ID>', views.details_books, name="details_books"),
+    path('area/', views.AreaInfo, name="areas"),
+    path('login/', views.login, name="login"),
+    path('login_submit/', views.login_submit, name="login_submit")
+]
